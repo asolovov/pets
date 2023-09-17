8b0d3b849064b85810c4dc40c39bf2b791cfe336
asolovov <andre.solovov@gmail.com>
2023-09-17 21:23:42 +0300

    feat: logger singleton

---
+++ pkg/logger/logursLogger.go
@@ -0,0 +1,34 @@
+package logger
+
+import (
+	"os"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+	easy "github.com/t-tomalak/logrus-easy-formatter"
+)
+
+type logger struct {
+	*logrus.Logger
+}
+
+var once sync.Once
+var instance *logger
+
+func Log() *logger {
+	once.Do(func() {
+		log := logrus.New()
+
+		log.SetLevel(logrus.InfoLevel)
+
+		log.SetOutput(os.Stdout)
+		log.SetFormatter(&easy.Formatter{
+			TimestampFormat: "2006-01-02 15:04:05",
+			LogFormat:       "[%lvl%]: %time% - Pets-%layer%: %msg%\n",
+		})
+
+		instance = &logger{log}
+	})
+
+	return instance
+}
